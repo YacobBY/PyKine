721408da9b3daada3818bd2d2151f08b12940e6f
Unknown <yacob101@gmail.com>
2019-02-10 17:52:51 +0100

    smth broken

--- Segment.py
+++ Segment.py
@@ -59,10 +59,13 @@ class InverseKinematics:
             segment.startY = self.previousSegment.endY
             segment.setPreviousSegment(self.previousSegment)
         self.segments.append(segment)
+        print(segment.startX, segment.startY)
+        print(segment.endX, segment.endY)
         self.previousSegment = segment
 
     def addSegments(self):
         for x in range(0, self.amountOfSegments):
+            print(x)
             self.addNewSegment()
 
     def update(self, x, y):
@@ -75,16 +78,19 @@ class InverseKinematics:
                 segment.startX = segment.previousSegment.endX
                 segment.startY = segment.previousSegment.endY
 
-
+IK = InverseKinematics(3, 200, 200)
 def drawLine(event):
     # def __init__(self, totalSegments, beginX, beginY, graphics):
+    mouseX, mouseY = event.x, event.y
 
+    IK.update(mouseX,mouseY)
+    segments = IK.segments
     canvas.delete("all")
     # print("start line")
-    mouseX, mouseY = event.x, event.y
+
     if canvas.old_coords:
         x1, y1 = canvas.old_coords
-        segments = InverseKinematics(3, 200, 200).segments
+
         print(segments.__len__())
         for segment in segments:
             canvas.create_line(segment.startY, 200, mouseX, mouseY)
